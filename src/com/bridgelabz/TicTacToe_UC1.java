da9351aa99c6d130b6ba5e8c1f981ab941d4136e
Ganeshp0296 <Ganeshp457@gmail.com>
2022-05-30 11:30:00 +0530

    1st commit

---
+++ src/com/bridgelabz/TicTacToe_UC1.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class TicTacToe_UC1 {
+
+		/**
+		 * declare 1d array of char type
+		 */
+		static char[] gameBoard = new char[10];
+
+		/**
+		 * method for creating empty GameBoard
+		 */
+		public void createGameBoard() {
+			for (int a = 1; a < gameBoard.length; a++) {
+				gameBoard[a] = ' ';
+			}
+		}
+
+		public static void main(String[] args) {
+
+			TicTacToe_UC1 ticTacToeMain = new TicTacToe_UC1();
+
+			System.out.println("Welcome to TicTacToe Game");
+
+			ticTacToeMain.createGameBoard();
+		}
+
+	}
+
+
+
