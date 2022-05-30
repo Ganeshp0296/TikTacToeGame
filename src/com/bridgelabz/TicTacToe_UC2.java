409a4a12bc76fbdfb8b790fe7a9c75b64b0b7f58
Ganeshp0296 <Ganeshp457@gmail.com>
2022-05-30 12:23:35 +0530

    Adding uc2

---
+++ src/com/bridgelabz/TicTacToe_UC2.java
@@ -0,0 +1,57 @@
+package com.bridgelabz;
+import java.util.Scanner;
+
+public class TicTacToe_UC2 {
+
+public class TicTacToeGame {
+	char[] gameBoard = new char[10]; //declare 1d array of char type
+	public char Computerletter;
+	public char Playerletter;
+	Scanner scanner = new Scanner(System.in);
+
+	/**
+	 * method for creating empty GameBoard
+	 */
+	public void createGameBoard() {
+		for (int a = 1; a < gameBoard.length; a++) {
+			gameBoard[a] = ' ';
+		}
+	}
+	/**
+     * method for choose Letter X or O
+     */
+	public void selectLetter() {
+        int flag = 0;
+        while (flag == 0) {
+            System.out.print("\n Choose your Letter\n Press 1 -> 'X'\n Press 2 -> 'O'\n Enter your Choice -> ");
+            int choice = scanner.nextInt();
+            switch(choice) {
+            case 1:
+            	Playerletter = 'X';
+                Computerletter = 'O';
+                System.out.println("\n Player letter -> " + Playerletter);
+                System.out.println("Computer letter -> " + Computerletter);
+                flag = 1;
+            	break;
+            case 2:
+            	Playerletter = 'O';
+                Computerletter = 'X';
+                System.out.println("\nPlayer letter -> " + Playerletter);
+                System.out.println("Computer letter -> " + Computerletter);
+                flag = 2;
+                break;
+             default :
+            	 System.out.println("\n plz enter 1 or 2");
+             }
+        }
+	}
+
+	public static void main(String[] args) {
+
+		TicTacToe_UC1 ticTacToeMain = new TicTacToe_UC1();
+		System.out.println("Welcome to TicTacToe Game");
+		ticTacToeMain.createGameBoard();
+//		ticTacToeMain.selectLetter();
+	}
+  }
+}
